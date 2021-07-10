da3cd36b0f874c5d60321e92903d2f409e1f41ae
invisible0831 <amirsoli80@gmail.com>
2021-07-10 21:35:05 +0430

    four by four multiplier added

--- project/modules/fbf_multiplier.v
+++ project/modules/fbf_multiplier.v
@@ -1,219 +1,271 @@
-
-module fbf_multiplier
-#(
-	parameter FLOAT_SIZE = 32
-)
-(
-	input reg clk,
-	input reg reset,
-	input load,
-	input wire[16*FLOAT_SIZE-1:0] A,
-	input wire [16*FLOAT_SIZE-1:0] B,
-	output wire [16*FLOAT_SIZE-1:0] Res,
-	output wire ready
+module fbf_multiplier (
+    input A_stb,
+    input B_stb,
+    input clk,
+    input reset,
+    input result_ack,
+    input [32 * 4 * 4 - 1:0] A,
+    input [32 * 4 * 4 - 1:0] B,
+    output reg result_ready,
+    output reg [32 * 4 * 4 - 1:0] result
 );
-parameter s_IDLE = 3'b000;
-parameter s_INITIAL  = 3'b001;
-parameter s_ADD_UP_1 = 3'b010;
-parameter s_FIRSTSHIFT  = 3'b011;
-parameter s_ADD_UP_2 = 3'b100;
-parameter s_CLEANUP = 3'b101;
-
-reg[2:0] state = s_IDLE;
-reg out_ready = 1'b0;    
-
-reg[FLOAT_SIZE-1:0] A_1_1[3:0];
-reg[FLOAT_SIZE-1:0] A_1_2[3:0];
-reg[FLOAT_SIZE-1:0] A_2_1[3:0];
-reg[FLOAT_SIZE-1:0] A_2_2[3:0];
-
-reg[FLOAT_SIZE-1:0] B_1_1[3:0];
-reg[FLOAT_SIZE-1:0] B_1_2[3:0];
-reg[FLOAT_SIZE-1:0] B_2_1[3:0];
-reg[FLOAT_SIZE-1:0] B_2_2[3:0];
-
-reg[FLOAT_SIZE-1:0] Res_1_1[3:0];
-reg[FLOAT_SIZE-1:0] Res_1_2[3:0];
-reg[FLOAT_SIZE-1:0] Res_2_1[3:0];
-reg[FLOAT_SIZE-1:0] Res_2_2[3:0];
-
-wire [FLOAT_SIZE-1:0] mid_Res_1_1[3:0];
-wire [FLOAT_SIZE-1:0] mid_Res_1_2[3:0];
-wire [FLOAT_SIZE-1:0] mid_Res_2_1[3:0];
-wire [FLOAT_SIZE-1:0] mid_Res_2_2[3:0];
-
-wire [3:0] tbt_ready;
-reg [3:0] tbt_load;
-
-assign Res[1 * FLOAT_SIZE - 1:0 * FLOAT_SIZE] = Res_1_1[0];
-assign Res[2 * FLOAT_SIZE - 1:1 * FLOAT_SIZE] = Res_1_1[1];
-assign Res[3 * FLOAT_SIZE - 1:2 * FLOAT_SIZE] = Res_1_2[0];
-assign Res[4 * FLOAT_SIZE - 1:3 * FLOAT_SIZE] = Res_1_2[1];
-assign Res[5 * FLOAT_SIZE - 1:4 * FLOAT_SIZE] = Res_1_1[2];
-assign Res[6 * FLOAT_SIZE - 1:5 * FLOAT_SIZE] = Res_1_1[3];
-assign Res[7 * FLOAT_SIZE - 1:6 * FLOAT_SIZE] = Res_1_2[2];
-assign Res[8 * FLOAT_SIZE - 1:7 * FLOAT_SIZE] = Res_1_2[3];
-
-assign Res[9 * FLOAT_SIZE - 1:8 * FLOAT_SIZE] = Res_2_1[0];
-assign Res[10 * FLOAT_SIZE - 1:9 * FLOAT_SIZE] = Res_2_1[1];
-assign Res[11 * FLOAT_SIZE - 1:10 * FLOAT_SIZE] = Res_2_2[0];
-assign Res[12 * FLOAT_SIZE - 1:11 * FLOAT_SIZE] = Res_2_2[1];
-assign Res[13 * FLOAT_SIZE - 1:12 * FLOAT_SIZE] = Res_2_1[2];
-assign Res[14 * FLOAT_SIZE - 1:13 * FLOAT_SIZE] = Res_2_1[3];
-assign Res[15 * FLOAT_SIZE - 1:14 * FLOAT_SIZE] = Res_2_2[2];
-assign Res[16 * FLOAT_SIZE - 1:15 * FLOAT_SIZE] = Res_2_2[3];
-
-
-
-assign  ready = out_ready;
-
-tbt_mult tbt_mul_1_1(
-	.clk(clk),
-	.reset(reset),
-	.A({A_1_1[3], A_1_1[2], A_1_1[1], A_1_1[0]}),
-	.B({B_1_1[3], B_1_1[2], B_1_1[1], B_1_1[0]}),
-	.Res({mid_Res_1_1[3], mid_Res_1_1[2], mid_Res_1_1[1], mid_Res_1_1[0]}),
-	.result_ready(tbt_ready[0]),
-	.load(tbt_load[0])
-);
-
-tbt_mult tbt_mul_1_2(
-	.clk(clk),
-	.reset(reset),
-	.A({A_1_2[3], A_1_2[2], A_1_2[1], A_1_2[0]}),
-	.B({B_1_2[3], B_1_2[2], B_1_2[1], B_1_2[0]}),
-	.Res({mid_Res_1_2[3], mid_Res_1_2[2], mid_Res_1_2[1], mid_Res_1_2[0]}),
-	.result_ready(tbt_ready[1]),
-	.load(tbt_load[1])
-);
-
-tbt_mult tbt_mul_2_1(
-	.clk(clk),
-	.reset(reset),
-	.A({A_2_1[3], A_2_1[2], A_2_1[1], A_2_1[0]}),
-	.B({B_2_1[3], B_2_1[2], B_2_1[1], B_2_1[0]}),
-	.Res({mid_Res_2_1[3], mid_Res_2_1[2], mid_Res_2_1[1], mid_Res_2_1[0]}),
-	.result_ready(tbt_ready[2]),
-	.load(tbt_load[2])
-);
-
-tbt_mult tbt_mul_2_2(
-	.clk(clk),
-	.reset(reset),
-	.A({A_2_2[3], A_2_2[2], A_2_2[1], A_2_2[0]}),
-	.B({B_2_2[3], B_2_2[2], B_2_2[1], B_2_2[0]}),
-	.Res({mid_Res_2_2[3], mid_Res_2_2[2], mid_Res_2_2[1], mid_Res_2_2[0]}),
-	.result_ready(tbt_ready[3]),
-	.load(tbt_load[3])
-);
-
-
-always @(posedge clk or negedge reset)
-    begin
-	state = state;
-	case(state)
-	   s_IDLE:
-		if (load)
-			state <= s_INITIAL;
-		else
-		        state <= s_IDLE;
-	   s_INITIAL:
-		begin
-		{A_1_1[3], A_1_1[2], A_1_1[1], A_1_1[0]} <= {A[6 * FLOAT_SIZE - 1:5 * FLOAT_SIZE], A[5 * FLOAT_SIZE - 1:4 * FLOAT_SIZE],A[2 * FLOAT_SIZE - 1:1 * FLOAT_SIZE], A[1 * FLOAT_SIZE - 1:0 * FLOAT_SIZE]};
-		{A_1_2[3], A_1_2[2], A_1_2[1], A_1_2[0]} <= {A[8 * FLOAT_SIZE - 1:7 * FLOAT_SIZE], A[7 * FLOAT_SIZE - 1:6 * FLOAT_SIZE],A[4 * FLOAT_SIZE - 1:3 * FLOAT_SIZE], A[3 * FLOAT_SIZE - 1:2 * FLOAT_SIZE]};
-		{A_2_1[3], A_2_1[2], A_2_1[1], A_2_1[0]} <= {A[16 * FLOAT_SIZE - 1:15 * FLOAT_SIZE], A[15 * FLOAT_SIZE - 1:14 * FLOAT_SIZE],A[12 * FLOAT_SIZE - 1:11 * FLOAT_SIZE], A[11 * FLOAT_SIZE - 1:10 * FLOAT_SIZE]};
-		{A_2_2[3], A_2_2[2], A_2_2[1], A_2_2[0]} <= {A[14 * FLOAT_SIZE - 1:13 * FLOAT_SIZE], A[13 * FLOAT_SIZE - 1:12 * FLOAT_SIZE],A[10 * FLOAT_SIZE - 1:9 * FLOAT_SIZE], A[9 * FLOAT_SIZE - 1:8 * FLOAT_SIZE]};
-		
-		{B_1_1[3], B_1_1[2], B_1_1[1], B_1_1[0]} <= {B[6 * FLOAT_SIZE - 1:5 * FLOAT_SIZE], B[5 * FLOAT_SIZE - 1:4 * FLOAT_SIZE],B[2 * FLOAT_SIZE - 1:1 * FLOAT_SIZE], B[1 * FLOAT_SIZE - 1:0 * FLOAT_SIZE]};
-		{B_1_2[3], B_1_2[2], B_1_2[1], B_1_2[0]} <= {B[8 * FLOAT_SIZE - 1:7 * FLOAT_SIZE], B[7 * FLOAT_SIZE - 1:6 * FLOAT_SIZE],B[4 * FLOAT_SIZE - 1:3 * FLOAT_SIZE], B[3 * FLOAT_SIZE - 1:2 * FLOAT_SIZE]};
-		{B_2_1[3], B_2_1[2], B_2_1[1], B_2_1[0]} <= {B[16 * FLOAT_SIZE - 1:15 * FLOAT_SIZE], B[15 * FLOAT_SIZE - 1:14 * FLOAT_SIZE],B[12 * FLOAT_SIZE - 1:11 * FLOAT_SIZE], B[11 * FLOAT_SIZE - 1:10 * FLOAT_SIZE]};
-		{B_2_2[3], B_2_2[2], B_2_2[1], B_2_2[0]} <= {B[14 * FLOAT_SIZE - 1:13 * FLOAT_SIZE], B[13 * FLOAT_SIZE - 1:12 * FLOAT_SIZE],B[10 * FLOAT_SIZE - 1:9 * FLOAT_SIZE], B[9 * FLOAT_SIZE - 1:8 * FLOAT_SIZE]};
-		{tbt_load[3], tbt_load[2], tbt_load[1], tbt_load[0]} <= 4'b1111;
-		state <= s_ADD_UP_1;
-		end
-	   s_ADD_UP_1:
-		begin
-		if(tbt_ready[0] && tbt_ready[1] && tbt_ready[2] && tbt_ready[3])
-			begin
-			Res_1_1[0] <= mid_Res_1_1[0];
-			Res_1_1[1] <= mid_Res_1_1[1];
-			Res_1_1[2] <= mid_Res_1_1[2];
-			Res_1_1[3] <= mid_Res_1_1[3];
-			
-			Res_1_2[0] <= mid_Res_1_2[0];
-			Res_1_2[1] <= mid_Res_1_2[1];
-			Res_1_2[2] <= mid_Res_1_2[2];
-			Res_1_2[3] <= mid_Res_1_2[3];
-			
-			Res_2_1[0] <= mid_Res_2_1[0];
-			Res_2_1[1] <= mid_Res_2_1[1];
-			Res_2_1[2] <= mid_Res_2_1[2];
-			Res_2_1[3] <= mid_Res_2_1[3];
-			
-			Res_2_2[0] <= mid_Res_2_2[0];
-			Res_2_2[1] <= mid_Res_2_2[1];
-			Res_2_2[2] <= mid_Res_2_2[2];
-			Res_2_2[3] <= mid_Res_2_2[3];
-		    state <= s_FIRSTSHIFT;
-		    {tbt_load[3], tbt_load[2], tbt_load[1], tbt_load[0]} <= 4'b0000;
-			end
-		end
-	   s_FIRSTSHIFT:
-		begin
-		{A_1_1[3], A_1_1[2], A_1_1[1], A_1_1[0]} <= {A[8 * FLOAT_SIZE - 1:7 * FLOAT_SIZE], A[7 * FLOAT_SIZE - 1:6 * FLOAT_SIZE],A[4 * FLOAT_SIZE - 1:3 * FLOAT_SIZE], A[3 * FLOAT_SIZE - 1:2 * FLOAT_SIZE]};
-		{A_1_2[3], A_1_2[2], A_1_2[1], A_1_2[0]} <= {A[6 * FLOAT_SIZE - 1:5 * FLOAT_SIZE], A[5 * FLOAT_SIZE - 1:4 * FLOAT_SIZE],A[2 * FLOAT_SIZE - 1:1 * FLOAT_SIZE], A[1 * FLOAT_SIZE - 1:0 * FLOAT_SIZE]};
-		{A_2_1[3], A_2_1[2], A_2_1[1], A_2_1[0]} <= {A[14 * FLOAT_SIZE - 1:13 * FLOAT_SIZE], A[13 * FLOAT_SIZE - 1:12 * FLOAT_SIZE],A[10 * FLOAT_SIZE - 1:9 * FLOAT_SIZE], A[9 * FLOAT_SIZE - 1:8 * FLOAT_SIZE]};
-		{A_2_2[3], A_2_2[2], A_2_2[1], A_2_2[0]} <= {A[16 * FLOAT_SIZE - 1:15 * FLOAT_SIZE], A[15 * FLOAT_SIZE - 1:14 * FLOAT_SIZE],A[12 * FLOAT_SIZE - 1:11 * FLOAT_SIZE], A[11 * FLOAT_SIZE - 1:10 * FLOAT_SIZE]};
-		
-		{B_1_1[3], B_1_1[2], B_1_1[1], B_1_1[0]} <= {B[16 * FLOAT_SIZE - 1:15 * FLOAT_SIZE], B[15 * FLOAT_SIZE - 1:14 * FLOAT_SIZE],B[12 * FLOAT_SIZE - 1:11 * FLOAT_SIZE], B[11 * FLOAT_SIZE - 1:10 * FLOAT_SIZE]};
-		{B_1_2[3], B_1_2[2], B_1_2[1], B_1_2[0]} <= {B[14 * FLOAT_SIZE - 1:13 * FLOAT_SIZE], B[13 * FLOAT_SIZE - 1:12 * FLOAT_SIZE],B[10 * FLOAT_SIZE - 1:9 * FLOAT_SIZE], B[9 * FLOAT_SIZE - 1:8 * FLOAT_SIZE]};
-		{B_2_1[3], B_2_1[2], B_2_1[1], B_2_1[0]} <= {B[6 * FLOAT_SIZE - 1:5 * FLOAT_SIZE], B[5 * FLOAT_SIZE - 1:4 * FLOAT_SIZE],B[2 * FLOAT_SIZE - 1:1 * FLOAT_SIZE], B[1 * FLOAT_SIZE - 1:0 * FLOAT_SIZE]};
-		{B_2_2[3], B_2_2[2], B_2_2[1], B_2_2[0]} <= {B[8 * FLOAT_SIZE - 1:7 * FLOAT_SIZE], B[7 * FLOAT_SIZE - 1:6 * FLOAT_SIZE],B[4 * FLOAT_SIZE - 1:3 * FLOAT_SIZE], B[3 * FLOAT_SIZE - 1:2 * FLOAT_SIZE]};
-		{tbt_load[3], tbt_load[2], tbt_load[1], tbt_load[0]} <= 4'b1111;
-		state <= s_ADD_UP_2;
-		end
-	   s_ADD_UP_2:
-		begin
-		if(tbt_ready[0])
-		   begin
-			Res_1_1[0] <= Res_1_1[0] + mid_Res_1_1[0];
-			Res_1_1[1] <= Res_1_1[1] + mid_Res_1_1[1];
-			Res_1_1[2] <= Res_1_1[2] + mid_Res_1_1[2];
-			Res_1_1[3] <= Res_1_1[3] + mid_Res_1_1[3];
-		   end
-		if(tbt_ready[1])
-		   begin
-			Res_1_2[0] <= Res_1_2[0] + mid_Res_1_2[0];
-			Res_1_2[1] <= Res_1_2[1] + mid_Res_1_2[1];
-			Res_1_2[2] <= Res_1_2[2] + mid_Res_1_2[2];
-			Res_1_2[3] <= Res_1_2[3] + mid_Res_1_2[3];
-		   end
-		if(tbt_ready[2])
-		   begin
-			Res_2_1[0] <= Res_2_1[0] + mid_Res_2_1[0];
-			Res_2_1[1] <= Res_2_1[1] + mid_Res_2_1[1];
-			Res_2_1[2] <= Res_2_1[2] + mid_Res_2_1[2];
-			Res_2_1[3] <= Res_2_1[3] + mid_Res_2_1[3];
-		   end
-		if(tbt_ready[3])
-		   begin
-			Res_2_2[0] <= Res_2_2[0] + mid_Res_2_2[0];
-			Res_2_2[1] <= Res_2_2[1] + mid_Res_2_2[1];
-			Res_2_2[2] <= Res_2_2[2] + mid_Res_2_2[2];
-			Res_2_2[3] <= Res_2_2[3] + mid_Res_2_2[3];
-		   end
-		if(tbt_ready[0] && tbt_ready[1] && tbt_ready[2] && tbt_ready[3])
-		   state <= s_CLEANUP;	
-		   {tbt_load[3], tbt_load[2], tbt_load[1], tbt_load[0]} <= 4'b0000;
-		end
-	   s_CLEANUP:
-		begin
-		out_ready <= 1'b1;
-		state <= s_IDLE;
-		end
-	endcase
+    
+    localparam UP = 1'b1;
+    localparam DOWN = 1'b0;
+    localparam LEFT = 1'b1;
+    localparam RIGHT = 1'b0;
+    
+    localparam S_IDLE = 0;
+    localparam S_SET_MULTIPLIERS_INPUT = 1;
+    localparam S_WAIT_FOR_FIRST = 2;
+    localparam S_SHIFT = 3;
+    localparam S_WAIT_FOR_SECOND = 4;
+    localparam S_SET_ADDER_INPUT = 5;
+    localparam S_WAIT_FOR_ADDER = 6;
+    localparam S_SET_RESULT = 7;
+    
+    reg [3:0] state = 0;
+    
+    reg [31:0] A_reg [3:0][3:0];
+    reg [31:0] B_reg [3:0][3:0];
+    reg [31:0] result_reg [3:0][3:0];
+    
+    wire [31:0] temp_mult_result [3:0][3:0];
+    reg [31:0] first_mult_result [3:0][3:0];
+    reg [31:0] second_mult_result [3:0][3:0];
+    wire mult_result_ready[3:0];
+    reg mult_load = 0;
+    reg mult_ready = 0;
+    
+    reg add_stb = 0;
+    reg add_result_ack = 0;
+    wire add_result_ready[3:0];
+    wire [31:0] temp_add_result [3:0][3:0];
+    
+    integer j;
+    integer k;
+    
+    genvar i;
+    generate for(i = 0; i < 4; i = i + 1)
+    begin: mult_loop
+        reg [3:0] row_index = (i / 2) * 2;
+        reg [3:0] column_index = (i % 2) * 2;
+        wire [32 * 2 * 2 - 1:0] input_A, input_B, output_result;
+        assign input_A = {A_reg[row_index + 1][column_index + 1], A_reg[row_index + 1][column_index], A_reg[row_index][column_index + 1], A_reg[row_index][column_index]};
+        assign input_B = {B_reg[row_index + 1][column_index + 1], B_reg[row_index + 1][column_index], B_reg[row_index][column_index + 1], B_reg[row_index][column_index]};
+        tbt_multiplier multiplier(
+            .clk(clk),
+            .reset(reset),
+            .load(mult_load),
+            .A(input_A),
+            .B(input_B),
+            .Res(output_result),
+            .result_ready(mult_result_ready[i])
+        );
+        assign temp_mult_result[row_index][column_index] = output_result[31:0];
+        assign temp_mult_result[row_index][column_index + 1] = output_result[63:32];
+        assign temp_mult_result[row_index + 1][column_index] = output_result[95:64];
+        assign temp_mult_result[row_index + 1][column_index + 1] = output_result[127:96];
     end
-
-
-endmodule
-
+    endgenerate
+    
+    genvar l;
+    generate for(l = 0; l < 4; l = l + 1)
+    begin: add_loop 
+        reg [3:0] row_index = (l / 2) * 2;
+        reg [3:0] column_index = (l % 2) * 2;
+        wire [32 * 2 * 2 - 1:0] input_A, input_B, output_result;
+        assign input_A = {first_mult_result[row_index + 1][column_index + 1], first_mult_result[row_index + 1][column_index], first_mult_result[row_index][column_index + 1], first_mult_result[row_index][column_index]};
+        assign input_B = {second_mult_result[row_index + 1][column_index + 1], second_mult_result[row_index + 1][column_index], second_mult_result[row_index][column_index + 1], second_mult_result[row_index][column_index]};
+        tbt_adder adder (
+            .A_stb(add_stb),
+            .B_stb(add_stb),
+            .clk(clk),
+            .reset(reset),
+            .result_ack(add_result_ack),
+            .A(input_A),
+            .B(input_B),
+            .result_ready(add_result_ready[l]),
+            .result(output_result)
+        );
+        assign temp_add_result[row_index][column_index] = output_result[31:0];
+        assign temp_add_result[row_index][column_index + 1] = output_result[63:32];
+        assign temp_add_result[row_index + 1][column_index] = output_result[95:64];
+        assign temp_add_result[row_index + 1][column_index + 1] = output_result[127:96];
+    end
+    endgenerate
+    
+    always @ (posedge clk or negedge reset)
+    begin
+        if(!reset)
+        begin
+            
+        end
+        else
+        begin
+            case (state):
+                S_IDLE:
+                begin
+                    result_ready <= 0;
+                    if(A_stb && B_stb)
+                    begin
+                        set_A_and_B();
+                        shift_left_block_A(DOWN);
+                        shift_left_block_B(RIGHT);
+                        state <= S_SET_MULTIPLIERS_INPUT;
+                    end
+                    else
+                    begin
+                        state <= S_IDLE;
+                    end
+                end
+                S_SET_MULTIPLIERS_INPUT:
+                begin
+                    mult_load <= 1;
+                    state <= S_WAIT_FOR_FIRST;
+                end
+                S_WAIT_FOR_FIRST:
+                begin
+                    mult_load <= 0;
+                    if(mult_result_ready[0] && mult_result_ready[1] && mult_result_ready[2] && mult_result_ready[3])
+                    begin
+                        for(j = 0; j < 4; j = j + 1)
+                        begin
+                            for(k = 0; k < 4; k = k + 1)
+                            begin
+                                first_mult_result[j][k] <= temp_mult_result[j][k];
+                            end
+                        end
+                    end
+                    state <= S_SHIFT;
+                end
+                S_SHIFT:
+                begin
+                    shift_left_block_A(UP);
+                    shift_left_block_A(DOWN);
+                    shift_left_block_B(LEFT);
+                    shift_left_block_B(RIGHT);
+                    mult_load <= 1;
+                    state <= S_WAIT_FOR_SECOND;
+                end
+                S_WAIT_FOR_SECOND:
+                begin
+                    mult_load <= 0;
+                    if(mult_result_ready[0] && mult_result_ready[1] && mult_result_ready[2] && mult_result_ready[3])
+                    begin
+                        for(j = 0; j < 4; j = j + 1)
+                        begin
+                            for(k = 0; k < 4; k = k + 1)
+                            begin
+                                second_mult_result[j][k] <= temp_mult_result[j][k];
+                            end
+                        end
+                    end 
+                    state <= S_SET_ADDER_INPUT;
+                end
+                S_SET_ADDER_INPUT:
+                begin
+                    add_stb <= 1;
+                    state <= S_WAIT_FOR_ADDER;
+                end
+                S_WAIT_FOR_ADDER:
+                begin
+                    add_stb <= 0;
+                    if(add_result_ready[0] && add_result_ready[1] && add_result_ready[2] && add_result_ready[3])
+                    begin
+                        for(j = 0; j < 4; j = j + 1)
+                        begin
+                            for(k = 0; k < 4; k = k + 1)
+                            begin
+                                result_reg[j][k] <= temp_add_result[j][k];
+                            end
+                        end
+                    end
+                    add_result_ack <= 1;
+                    state <= S_SET_RESULT;
+                end
+                S_SET_RESULT:
+                begin
+                    add_result_ack <= 0;
+                    set_result();
+                    result_ready <= 1;
+                    if(result_ack)
+                    begin
+                        result_ready <= 0;
+                        state <= S_IDLE;
+                    end
+                    else
+                    begin
+                        state <= S_SET_RESULT;
+                    end 
+                end
+                default:
+                begin
+                    state <= S_IDLE;
+                end
+            endcase
+        end
+    end
+    
+    task automatic set_A_and_B();
+    begin
+        integer m, n;
+        for(m = 0; m < 4 ; m = m + 1)
+        begin
+            for(n = 0; n < 4 ; n = n + 1)
+            begin
+                A_reg[m][n] = A[((m*2+n)*32)+:32];
+                B_reg[m][n] = B[((m*2+n)*32)+:32];
+            end
+        end
+    end
+    endtask
+    
+    task automatic shift_left_block_A(
+        input up
+    );
+    begin
+        reg [1:0] offset;
+        offset = up ? 2'd0 : 2'd2;
+        A_reg[offset][0] <= A_reg[offset][2];
+        A_reg[offset][2] <= A_reg[offset][0];
+        A_reg[offset][1] <= A_reg[offset][3];
+        A_reg[offset][3] <= A_reg[offset][1];
+        A_reg[offset + 1][0] <= A_reg[offset + 1][2];
+        A_reg[offset + 1][2] <= A_reg[offset + 1][0];
+        A_reg[offset + 1][1] <= A_reg[offset + 1][3];
+        A_reg[offset + 1][3] <= A_reg[offset + 1][1];
+    end
+    endtask
+    
+    task automatic shift_left_block_B(
+        input left
+    );
+    begin
+        reg [1:0] offset;
+        offset = left ? 2'd0 : 2'd2;
+        B_reg[0][offset] <= B_reg[2][offset];
+        B_reg[2][offset] <= B_reg[0][offset];
+        B_reg[1][offset] <= B_reg[3][offset];
+        B_reg[3][offset] <= B_reg[1][offset];
+        B_reg[0][offset + 1] <= B_reg[2][offset + 1];
+        B_reg[2][offset + 1] <= B_reg[0][offset + 1];
+        B_reg[1][offset + 1] <= B_reg[3][offset + 1];
+        B_reg[3][offset + 1] <= B_reg[1][offset + 1];
+    end
+    endtask
+    
+    task automatic set_result();
+    begin
+        integer m, n;
+        for(m = 0; m < 4; m = m + 1)
+        begin
+            for(n = 0; n < 4; n = n + 1)
+            begin
+                result[((4*m+n)*32)+:32] = result_reg[m][n];
+            end
+        end
+    end
+    endtask
+    
+ endmodule
