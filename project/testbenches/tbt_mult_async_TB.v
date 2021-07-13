4a9af52cfe925b99ef0c92453d5b302f10e8a58a
solale427 <seiedesolale@gmail.com>
2021-07-13 21:00:48 +0430

    santez top

--- project/testbenches/tbt_mult_async_TB.v
+++ project/testbenches/tbt_mult_async_TB.v
@@ -1,205 +1,66 @@
-module tbt_mult_async
-# (
-	parameter FLOATSIZE = 32
-)
-(
-	input clk,
-	input reset,
-	input load,
-	input [2 * 2 * FLOATSIZE - 1:0] A,
-	input [2 * 2 * FLOATSIZE - 1:0] B,
-	input result_ack,
-	output reg [2 * 2 * FLOATSIZE - 1:0] result,
-	output result_ready
-);
+`include "tbt_multiplier_async.v"
+`timescale 1ns / 1ps
 
-localparam s_IDLE = 0;
-localparam s_INITIALIZE = 1;
-localparam s_MULT = 2;
-localparam s_WAIT = 3;
-localparam s_SUM = 4;
-localparam s_SET_RESULT = 5;
-localparam s_GET_RESULT = 6;
+module tbt_mult_async_TB();
+reg clk = 0;
+reg reset = 1;
+reg [127:0] A;
+reg [127:0] B;
+wire [127:0] generated_mul;
+reg [127:0] correct_mul;
+wire result_ready;
+wire result_ack;
+reg load = 1'b1;
+reg [31:0] A_reg [0:1][0:1];
+reg [31:0] B_reg [0:1][0:1];
+reg [31:0] Res_reg [0:1][0:1];
 
-//internal variables
-reg [FLOATSIZE - 1:0] A_reg	[1:0][1:0];
-reg [FLOATSIZE - 1:0] B_reg	[1:0][1:0];
-reg [FLOATSIZE - 1:0] res_reg [1:0][1:0];
-reg [FLOATSIZE - 1:0] input_adder_a;
-reg [FLOATSIZE - 1:0] input_mult_a [0:1];
-reg [FLOATSIZE - 1:0] input_mult_b [0:1];
-reg [FLOATSIZE - 1:0] state	= s_IDLE;
-reg result_ready_reg = 0;
-reg input_mult_a_stb [0:1];
-reg input_mult_b_stb [0:1];
-reg input_mult_reset = 1;
-reg input_adder_load = 0;
-reg input_adder_reset = 0;
-reg input_adder_result_ack = 0;
+tbt_mult_async uut(
+        .clk(clk),
+        .reset(reset),
+		.load(load),
+        .A(A),
+        .B(B),
+		.result_ack(result_ack),
+        .result(generated_mul),
+        .result_ready(result_ready)
+    );
 
-wire [FLOATSIZE - 1:0] output_adder_result;
-wire [FLOATSIZE - 1:0] output_mult_result [0:1];
-wire output_adder_ready;
-wire output_mult_ready [0:1];
 
-genvar m, n;
-integer i, j, k = 0;
+always #10 clk = ~clk;
 
-generate
-begin
-	for (n = 0; n < 2; n = n + 1)
-		single_multiplier mult(
-			.input_a(input_mult_a[n]),
-			.input_b(input_mult_b[n]),
-			.input_a_stb(input_mult_a_stb[n]),
-			.input_b_stb(input_mult_b_stb[n]),
-			.output_z_ack(),
-			.clk(clk),
-			.rst(input_mult_reset),
-			.output_z(output_mult_result[n]),
-			.output_z_stb(output_mult_ready[n]),
-			.input_a_ack(),
-			.input_b_ack()
-		);
-		
+initial begin
+	$monitor ("A = %h, B = %h, Res = %h, valid result: %h, result ready: %b", A, B, generated_mul, correct_mul, result_ready);
+	//	A = | 5.84   8.16 |
+	//		| -3.01  -10  |
+	//
+	//	B = | 20.9   -12.8|
+	//		| 9.35   2    |
+	//	
+	//	Res = | 198.352  -58.432|
+	//		  | -156.409  18.528|
 	
+	A_reg[0][0] = 32'b01000000101110101110000101001000;
+	A_reg[0][1]	= 32'b01000001000000101000111101011100;
+	A_reg[1][0]	= 32'b11000000010000001010001111010111;
+	A_reg[1][1]	= 32'b11000001001000000000000000000000;
+	
+	B_reg[0][0]	= 32'b01000001101001110011001100110011;
+	B_reg[0][1]	= 32'b11000001010011001100110011001101;
+	B_reg[1][0]	= 32'b01000001000101011001100110011010;
+	B_reg[1][1]	= 32'b01000000000000000000000000000000;
+	
+	Res_reg[0][0]	= 32'b01000011010001100101101000011101;
+	Res_reg[0][1]	= 32'b11000010011010011011101001011110;
+	Res_reg[1][0]	= 32'b11000011000111000110100010110100;
+	Res_reg[1][1]	= 32'b01000001100101000011100101011000;
+	
+	A = {A_reg[0][0], A_reg[0][1], A_reg[1][0], A_reg[1][1]};
+	B = {B_reg[0][0], B_reg[0][1], B_reg[1][0], B_reg[1][1]};
+	
+	correct_mul = {Res_reg[0][0], Res_reg[0][1], Res_reg[1][0], Res_reg[1][1]};
 
-		
-end
-endgenerate
 
-adder single_adder(
-		.clk(clk),
-		.reset(input_adder_reset), 
-		.load(input_adder_load),
-		.Number1(output_mult_result[0]), 
-		.Number2(output_mult_result[1]),
-		.result_ack(input_adder_result_ack),
-		.Result(output_adder_result),
-		.result_ready(output_adder_ready)
-);
-
-assign result_ready = result_ready_reg;
-
-always @(posedge clk or negedge reset)
-begin
-	if (~reset)
-	begin
-		state <= s_IDLE;
-		input_adder_load <= 1'b0;
-		input_adder_reset <= 1'b0;
-		input_mult_reset <= 1'b1;
-		result_ready_reg <= 1'b0;
-		for (i = 0; i < 2; i = i + 1)
-		begin
-			input_mult_a_stb[i] <= 1'b0;
-			input_mult_a_stb[i] <= 1'b0;
-		end
-	end
-	else
-	begin
-		case(state)
-			s_IDLE:
-			begin
-				input_adder_load <= 1'b0;
-				input_adder_reset <= 1'b0;
-				input_mult_reset <= 1'b1;
-				i <= 0;
-				j <= 0;
-				for (k = 0; k < 2; k = k + 1)
-				begin
-					input_mult_a_stb[k] <= 1'b0;
-					input_mult_a_stb[k] <= 1'b0;
-				end
-				if(load)
-				begin
-					{A_reg[1][1], A_reg[1][0], A_reg[0][1], A_reg[0][0]} = A;
-					{B_reg[1][1], B_reg[1][0], B_reg[0][1], B_reg[0][0]} = B;
-					state <= s_INITIALIZE;
-				end
-				else
-					state <= s_IDLE;
-			end
-			s_INITIALIZE:
-			begin
-				input_mult_a[0] <= A_reg[i][0];
-				input_mult_b[0] <= B_reg[0][j];
-				input_mult_a[1] <= A_reg[i][1];
-				input_mult_b[1] <= B_reg[1][j];
-				input_mult_reset <= 0;
-				state <= s_MULT;
-			end
-			s_MULT:
-			begin
-				input_mult_a_stb[0] <= 1;
-				input_mult_a_stb[1] <= 1;
-				input_mult_b_stb[0] <= 1;
-				input_mult_b_stb[1] <= 1;
-				state <= s_WAIT;
-			end
-			s_WAIT:
-			begin
-				if (output_mult_ready[0] && output_mult_ready[1])
-				begin
-					input_adder_reset <= 1;
-					state <= s_SUM;
-				end
-				else
-					state <= s_WAIT;
-			end
-			s_SUM:
-			begin
-				input_adder_load <= 1;
-				if (output_adder_ready)
-				begin
-					res_reg[i][j] <= output_adder_result;
-					state <= s_SET_RESULT;
-				end
-				else
-					state <= s_SUM;
-			end
-			s_SET_RESULT:
-			begin
-				input_adder_result_ack <= 1;
-				if (i >= 1 && j >= 1)
-				begin
-					result_ready_reg <= 1;
-					result = {res_reg[1][1], res_reg[1][0], res_reg[0][1], res_reg[0][0]};
-					state <= s_GET_RESULT;
-				end
-				else
-				begin
-					j <= j + 1;
-					if (j >= 1)
-					begin
-						j <= 0;
-						i <= i + 1;
-					end
-					input_adder_reset <= 0;
-					input_mult_reset <= 1;
-					input_adder_load <= 0;
-					input_adder_result_ack <= 0;
-					for (k = 0; k < 2; k = k + 1)
-					begin
-						input_mult_a_stb[k] <= 1'b0;
-						input_mult_b_stb[k] <= 1'b0;
-					end
-					state <= s_INITIALIZE;
-				end
-			end
-			s_GET_RESULT:
-				if (result_ack)
-				begin
-					result_ready_reg <= 1'b0;
-					input_adder_result_ack <= 0;
-					state <= s_IDLE;
-				end
-				else
-					state <= s_GET_RESULT;
-			default:
-				state <= s_IDLE;
-		endcase
-	end
-end
+    end
 
 endmodule
